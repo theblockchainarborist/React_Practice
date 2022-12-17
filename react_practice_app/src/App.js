d2091732fae8751fb512f2ab6dbe275938943070
theblockchainarborist <theblockchainarborist@gmail.com>
2022-12-17 15:53:45 -0500

    Added ColorToggleBtn Component

--- react_practice_app/src/App.js
+++ react_practice_app/src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import ColorToggleBtn from './components/buttons/btn-change-color-on-click';
+
 
 function App() {
   return (
@@ -18,6 +20,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <div>
+        <ColorToggleBtn />
+      </div>
     </div>
   );
 }
